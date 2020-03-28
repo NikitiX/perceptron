514ed1007b6e290eb1c7a92ace753740cbc58453
titovn439 <47417272+NikitiX@users.noreply.github.com>
2020-03-28 17:01:23 +0300

    Update perceptron.cpp

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,21 +1,6 @@
-#include <iostream>
-#include <vector>
+#include "perceptron.h"
 
 using namespace std;
-
-class Perceptron {
-public:
-    Perceptron(vector<double> f_coefficients);
-    int set_inputs(vector<double> a_inputs);
-    int set_input(int index, double value);
-    int set_coefficients(vector<double> a_coefficients);
-    int set_coefficient(int index, double value);
-    double get_output();
-private:
-    vector<double> inputs;
-    vector<double> coefficients;
-    int num_of_inputs;
-};
 
 Perceptron::Perceptron(vector<double> f_coefficients) {
     num_of_inputs = f_coefficients.size();
