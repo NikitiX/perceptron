e21329b184718e03ac38d4cad1b686c44944880a
NikitiX <47417272+NikitiX@users.noreply.github.com>
2020-03-28 16:49:13 +0300

    Update perceptron.cpp

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,1 +1,75 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+class Perceptron {
+public:
+    Perceptron(vector<double> f_coefficients);
+    int set_inputs(vector<double> a_inputs);
+    int set_input(int index, double value);
+    int set_coefficients(vector<double> a_coefficients);
+    int set_coefficient(int index, double value);
+    double get_output();
+private:
+    vector<double> inputs;
+    vector<double> coefficients;
+    int num_of_inputs;
+};
+
+Perceptron::Perceptron(vector<double> f_coefficients) {
+    num_of_inputs = f_coefficients.size();
+    coefficients = f_coefficients;
+};
+
+int Perceptron::set_inputs(vector<double> a_inputs) {
+    if (a_inputs.size() != num_of_inputs - 1) {
+        return 0;
+    }
+    inputs = a_inputs;
+    return 1;
+};
+
+int Perceptron::set_input(int index, double value) {
+    if ((index < 0) || (index >= num_of_inputs)) {
+        return 0;
+    }
+    inputs[index] = value;
+    return 1;
+};
+
+int Perceptron::set_coefficients(vector<double> a_coefficients) {
+    if (a_coefficients.size() != num_of_inputs) {
+        return 0;
+    }
+    coefficients = a_coefficients;
+    return 1;
+};
+
+int Perceptron::set_coefficient(int index, double value) {
+    if ((index < 0) || (index > num_of_inputs)) {
+        return 0;
+    }
+    coefficients[index] = value;
+    return 1;
+};
+
+double Perceptron::get_output() {
+    double result = coefficients[0];
+    for (int i = 0; i < num_of_inputs; i++) {
+        result += inputs[i] * coefficients[i+1];
+    }
+    return result;
+};
+
+int main()
+{
+    vector<double> c = {0.5,1.5,1};
+    vector<double> inputs = {0,0.5};
+    Perceptron p(c);
+    if (!p.set_inputs(inputs)) {
+        cout << "Failed to load inputs into the perceptron.";
+    } else {
+        cout << "The result is " << p.get_output();
+    };
+}
