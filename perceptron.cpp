98cd357b0290ede64e34acb87a2181a663b7bafd
titovn439 <47417272+NikitiX@users.noreply.github.com>
2020-03-28 18:31:03 +0300

    Add files via upload

--- perceptron.cpp
+++ perceptron.cpp
@@ -2,11 +2,17 @@
 
 using namespace std;
 
-Perceptron::Perceptron(int n) {
+double random() {
+    return static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
+};
+
+Perceptron::Perceptron(unsigned int n,double l=0.3,double e=0.001) {
     num_of_inputs = n;
     for (int i = 0; i < n; i++) {
-        coefficients.push_back(static_cast <float> (rand()) / static_cast <float> (RAND_MAX) - 0.5);
+        coefficients.push_back(random() - 0.5);
     }
+    learn_rate = l;
+    epsilon = e;
 };
 
 bool Perceptron::set_inputs(vector<double> a_inputs) {
@@ -14,25 +20,70 @@ bool Perceptron::set_inputs(vector<double> a_inputs) {
         return false;
     }
     inputs = a_inputs;
+    process();
     return true;
 };
 
-double Perceptron::get_output() {
-    double result = coefficients[0];
+double Perceptron::process() {
+    current_result = coefficients[0];
     for (int i = 0; i < num_of_inputs; i++) {
-        result += inputs[i] * coefficients[i+1];
+        current_result += inputs[i] * coefficients[i+1];
     }
-    return result;
+};
+
+double Perceptron::get_output() {
+    return current_result;
+};
+
+double Perceptron::activationFunction(double arg) {
+    return 1. / (1. + exp(-arg));
+};
+
+double Perceptron::dActivationFunction(double arg) {
+    return activationFunction(arg) * (1. - activationFunction(arg));
+};
+
+bool Perceptron::learn(double real_answer) {
+    double error = real_answer - current_result;
+    //cout << endl << error << endl;
+    if ((error<0?-error:error) < epsilon) {
+        return true;
+    }
+	double teta = error * activationFunction(current_result);
+	for (int i = 1; i < num_of_inputs; i++) {
+		coefficients[i] += learn_rate * teta * inputs[i-1];
+	};
+	coefficients[0] += learn_rate * teta;
+	return false;
+};
+
+void Perceptron::print() {
+    for (int i = 0; i < num_of_inputs; i++) {
+        cout << coefficients[i] << ' ';
+    }
+    cout << endl;
 };
 
 int main()
 {
-    srand (static_cast <unsigned> (time(0)));
-    vector<double> inputs = {0.5,1};
+    srand(static_cast <unsigned> (time(0)));
     Perceptron p(3);
-    if (!p.set_inputs(inputs)) {
-        cout << "Failed to load inputs into the perceptron.";
-    } else {
-        cout << "The result is " << p.get_output();
-    };
+    int right_answers = 0;
+    while (true) {
+        double a = random();
+        double b = random();
+        vector<double> inputs = {a,b};
+        //cout << "Learning a - (b/2): ";
+        //p.print();
+        p.set_inputs(inputs);
+        double real_answer = a - (b / 2.);
+        //cout << "Inputs: " << a << ' ' << b << ", the real answer is " << real_answer << ", the perceptron answer is " << p.get_output() << endl;
+        if (p.learn(real_answer)) {
+            if (++right_answers == 10) {
+                break;
+            }
+        } else {
+            right_answers = 0;
+        }
+    }
 }
--- perceptron.h
+++ perceptron.h
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <cmath>
 #include <ctime>
 
 using namespace std;
 
 class Perceptron {
 public:
-    Perceptron(int num_of_inputs);
+    Perceptron(unsigned int num_of_inputs,double learning_rate,double epsilon);
     bool set_inputs(vector<double> a_inputs);
+    bool learn(double real_answer);
     double get_output();
+    void print();
 private:
+    double process();
+    double activationFunction(double arg);
+    double dActivationFunction(double arg);
     vector<double> inputs;
     vector<double> coefficients;
-    int num_of_inputs;
+    unsigned int num_of_inputs;
+    double epsilon;
+    double learn_rate;
+    double current_result;
 };
